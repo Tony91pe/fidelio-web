ada18e7c2a9b4c9db6562c67909758a383688544
Antonio <antopiersa@gmail.com>
2026-04-11 12:19:12 +0200

    fix: correggi shops route

--- src/app/api/app/shops/route.ts
+++ src/app/api/app/shops/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
+
 export async function GET() {
-const shops = await db.shop.findMany({
-select: { id:true, name:true, category:true, city:true, address:true }
-})
-return NextResponse.json(shops)
+  const shops = await db.shop.findMany({
+    select: { id:true, name:true, category:true, city:true, address:true }
+  })
+  return NextResponse.json(shops)
 }
