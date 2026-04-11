3813b6c0cbfa72a30bdac16c91d3ee1f8631b441
Antonio <antopiersa@gmail.com>
2026-04-11 12:40:29 +0200

    fix: aggiungi afterSignInUrl al componente SignIn

--- src/app/(auth)/login/[[...rest]]/page.tsx
+++ src/app/(auth)/login/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#0D0D1A]">
-      <SignIn />
+      <SignIn afterSignInUrl="/dashboard" redirectUrl="/dashboard" />
     </div>
   )
 }
